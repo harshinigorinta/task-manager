ec4a3df92afbfbb4ee2577b48a7ea5c322cf9f8c
gorinta harshini <gorintaharshini4114@gmail.com>
2026-05-10 18:44:19 +0530

    fix task route 404

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
+app.use('/api/projects', taskRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/projects', memberRoutes);
 
